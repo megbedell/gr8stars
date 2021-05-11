1b01b4b167790b54781f468677bec2717233a992
megbedell <megbedell@gmail.com>
2021-05-11 10:28:42 -0700

    updated gr8stars input sample

--- hires_query.py
+++ hires_query.py
@@ -6,6 +6,11 @@ from astropy.coordinates import SkyCoord
 from pykoa.koa import Koa
 from tqdm.auto import tqdm
 
+sample_infile = 'Gr8stars_GaiaeDR3_TIC.tsv'
+sample_outfile = 'Gr8stars_GaiaeDR3_TIC_hires.csv'
+hires_obs_outfile = 'koa_out/all.csv'
+resume = 2121 # index of sample to start querying -- 0 by default
+
 def filter(rec):
     return rec[(rec['iodout']=='T') & (rec['specres'] >= 60000)]
 
@@ -14,38 +19,46 @@ def coadd_snr(rec):
     return np.sqrt(np.sum(snrs**2))
     
 
-if __name__ == "__main__": 
-    # read in sample:  
-    sample = Table.read('ollie_stars.tsv', format='ascii',  
-            delimiter='\t', header_start=1, data_start=3)
+if __name__ == "__main__":
+    if resume > 0:
+        # load up in-progress sample:
+        sample = Table.read(sample_outfile)
+        hires_obs = Table.read(hires_obs_outfile)
+    else:
+        # read input sample:  
+        sample = Table(np.genfromtxt(sample_infile, 
+                                     delimiter='\t', names=True, 
+                                     dtype=None, encoding=None))
 
-    sample['n_hires_obs_all'] = 0
-    sample['n_hires_obs_iodout'] = 0
-    sample['hires_snr'] = 0
+        sample['n_hires_obs_all'] = 0
+        sample['n_hires_obs_iodout'] = 0
+        sample['hires_snr'] = 0
 
-    # set up table from query on first target:
-    Koa.query_position('HIRES', 'circle {0} {1} 0.5'.format(sample[0]['RA_ICRS'], sample[0]['DE_ICRS']),
-                    outpath='koa_out/{0}.tbl'.format(sample[0]['Gaia Name'].replace(" ", "_")),
-                    overwrite=True)
-    rec = Table.read('koa_out/{0}.tbl'.format(sample[0]['Gaia Name'].replace(" ", "_")), format='ascii.ipac')
-    rec['Gaia Name'] = sample[0]['Gaia Name']
+        # set up table from query on first target:
+        Koa.query_position('HIRES', 'circle {0} {1} 0.5'.format(sample[0]['_RAJ2000'], sample[0]['_DEJ2000']),
+                        outpath='koa_out/{0}.tbl'.format(sample[0]['EDR3Name'].replace(" ", "_")),
+                        overwrite=True)
+        rec = Table.read('koa_out/{0}.tbl'.format(sample[0]['EDR3Name'].replace(" ", "_")), format='ascii.ipac')
+        rec['EDR3Name'] = sample[0]['EDR3Name']
 
-    hires_obs = rec
+        hires_obs = rec
 
     # populate table with others:
     for i,s in enumerate(sample):
+        if i<resume:
+            continue
         if i%10 == 0:
             print('TARGET {0}/{1}'.format(i+1, len(sample)))
         try: 
-            rec = Table.read('koa_out/{0}.tbl'.format(s['Gaia Name'].replace(" ", "_")), format='ascii.ipac')
+            rec = Table.read('koa_out/{0}.tbl'.format(s['EDR3Name'].replace(" ", "_")), format='ascii.ipac')
         except:
-            Koa.query_position('HIRES', 'circle {0} {1} 0.5'.format(s['RA_ICRS'], s['DE_ICRS']),
-                    outpath='koa_out/{0}.tbl'.format(s['Gaia Name'].replace(" ", "_")),
+            Koa.query_position('HIRES', 'circle {0} {1} 0.5'.format(s['_RAJ2000'], s['_DEJ2000']),
+                    outpath='koa_out/{0}.tbl'.format(s['EDR3Name'].replace(" ", "_")),
                     overwrite=True)
-            rec = Table.read('koa_out/{0}.tbl'.format(s['Gaia Name'].replace(" ", "_")), format='ascii.ipac')
+            rec = Table.read('koa_out/{0}.tbl'.format(s['EDR3Name'].replace(" ", "_")), format='ascii.ipac')
         sample[i]['n_hires_obs_all'] = len(rec)
         if len(rec['koaid']) >= 1:
-            rec['Gaia Name'] = s['Gaia Name']
+            rec['EDR3Name'] = s['EDR3Name']
             good = filter(rec)
             if len(good) >= 1:
                 sample[i]['n_hires_obs_iodout'] = len(good['koaid'])
@@ -53,5 +66,5 @@ if __name__ == "__main__":
             hires_obs = vstack([hires_obs, rec])
 
     # save:
-    hires_obs.write('koa_out/all.csv', overwrite=True)
-    sample.write('ollie_stars_w_hires.csv', overwrite=True) 
+    hires_obs.write(hires_obs_outfile, overwrite=True)
+    sample.write(sample_outfile, overwrite=True) 
